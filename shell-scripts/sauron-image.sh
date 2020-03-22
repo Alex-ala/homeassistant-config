11a86162826883bc3cb35c43faff9cb48d5e4a67
alex <9bpKUTM8e7zmVbOut0ID>
2020-03-22 20:19:47 +0100

    fix paths for images

--- shell-scripts/sauron-image.sh
+++ shell-scripts/sauron-image.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
 DATE=$(/bin/date "+%Y_%m_%d-%H_%M_%S")
-/usr/bin/ssh -i /home/homeassistant/.ssh/id_rsa smarthome@sauron raspistill -o - > /data/nogrod/homeassistant/images/ir/${DATE}.jpg
-/bin/ln -fs /data/nogrod/homeassistant/images/ir/${DATE}.jpg /data/nogrod/homeassistant/images/ir/latest.jpg
-/usr/bin/find /data/nogrod/homeassistant/images/ir/* -mtime +60 -delete
+/usr/bin/ssh -i /home/homeassistant/.ssh/id_rsa smarthome@sauron raspistill -o - > /data/nogrod/homeassistant/images/sauron/${DATE}.jpg
+/bin/ln -fs /data/nogrod/homeassistant/images/sauron/${DATE}.jpg /data/nogrod/homeassistant/images/sauron/latest.jpg
+/usr/bin/find /data/nogrod/homeassistant/images/sauron/* -mtime +60 -delete
